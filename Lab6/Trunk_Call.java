070060a533e0b30e4bb2d1fc8c80242c020fd711
AnanyaM12 <ananyaamathur03@gmail.com>
2022-10-29 00:42:08 +0530

    Lab6 is done

--- Lab6/Trunk_Call.java
+++ Lab6/Trunk_Call.java
@@ -1,7 +1,36 @@
 package Lab6;
-
 public class Trunk_Call 
 {
-    
-    
+    int ordinary_rate=10;
+    int urgent_rate=20;
+    int lighting_rate=30;
+    public static void main (String args[])
+    {
+        ordinary a =new ordinary();
+        a.charges(1);
+        urgent b =new urgent();
+        b.charges(2);
+        lighting c =new lighting();
+        c.charges(3);
+    }
+}
+class ordinary extends Trunk_Call
+{
+    void charges(int hours)
+    {
+        System.out.println(ordinary_rate*hours);
+    }
+}
+class urgent extends Trunk_Call{
+    void charges(int hours)
+    {
+        System.out.println(urgent_rate*hours);
+    }
+}
+class lighting extends Trunk_Call
+{
+    void charges (int hours)
+    {
+        System.out.println(lighting_rate*hours);
+    }
 }
